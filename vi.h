2e2bd35449db1bc813b1318226589d9027c82d49
Ian Jarvis <ijarvis12@gmail.com>
2023-12-10 12:48:51 -0600

    Update vi.h

--- vi.h
+++ vi.h
@@ -1,7 +1,4 @@
 #include <form.h>
-#include "vi_cmdmode.h"
-#include "vi_insmode.h"
-#include "vi_vismode.h"
 
 extern int maxy, maxx;          /* Maximum values of rows and columns */
 extern WINDOW *editor_window;   /* The editor window */
