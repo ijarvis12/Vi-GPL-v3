46cd57d02e210d761c4e34b3a7e3f3b3ac8830a1
Ian Jarvis <ijarvis12@gmail.com>
2023-12-10 14:02:10 -0600

    Update vi.h

--- vi.h
+++ vi.h
@@ -1,5 +1,6 @@
 #include <form.h>
 
-extern int maxy, maxx;          /* Maximum values of rows and columns */
-extern WINDOW *editor_window;   /* The editor window */
-extern FORM *editor_form;       /* The command line form/field */
+WINDOW *stdscr;                 /* The standard screen to draw on */
+int maxy, maxx;                 /* Maximum values of rows and columns */
+WINDOW *editor_window;          /* The editor window */
+FORM *editor_form;              /* The command line form/field */
