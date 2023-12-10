a49d9113bd68449bd7f7da5810a346a2268e2fe3
Ian Jarvis <ijarvis12@gmail.com>
2023-12-10 15:01:41 -0600

    Update vi_cmdmode.c

--- vi_cmdmode.c
+++ vi_cmdmode.c
@@ -3,5 +3,13 @@
 void
 cmd_main()
 {
+        /* Get command from window/field */
+        char command[maxx+1];
+        wgetnstr(cmd_window, command, maxx);
 
+        /* Process command */
+        char first_char = command[0];
+        switch (first_char) {
+
+        }
 }
