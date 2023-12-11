709f3ca20af100514d8f99d765f27e1d80f1cce6
Ian Jarvis <ijarvis12@gmail.com>
2023-12-11 12:39:17 -0600

    Update vi.h

--- vi.h
+++ vi.h
@@ -5,5 +5,14 @@ int maxy, maxx;                 /* Maximum values of screen rows and columns */
 WINDOW *editor_window;          /* The editor window */
 WINDOW *cmd_window;             /* The command line window/field */
 
+/* The text buffers */
+struct Buffers {
+        struct Buffer {
+                struct Lines {
+                        char[4096] line;
+                } lines[maxy-1];
+        } buffer[128];
+} buffers;
+
 extern void cmdmode_main();     /* Command mode main function */
 extern void visualmode_main();  /* Visual mode main function */
