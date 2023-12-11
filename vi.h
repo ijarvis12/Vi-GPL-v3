75e86c1371d112386162279fa54e1f39abe6907c
Ian Jarvis <ijarvis12@gmail.com>
2023-12-10 21:09:34 -0600

    Update vi.h

--- vi.h
+++ vi.h
@@ -4,3 +4,6 @@ WINDOW *stdscr;                 /* The standard screen to draw on */
 int maxy, maxx;                 /* Maximum values of rows and columns */
 WINDOW *editor_window;          /* The editor window */
 WINDOW *cmd_window;             /* The command line window/field */
+
+extern void cmdmode_main();     /* Command mode main function */
+extern void visualmode_main();  /* Visual mode main function */
