40053b2fb47c5caa36bfc47d116e3c6b817db5da
Ian Jarvis <ijarvis12@gmail.com>
2023-12-11 09:25:03 -0600

    Update vi.h

--- vi.h
+++ vi.h
@@ -1,7 +1,7 @@
 #include <ncurses.h>
 
 WINDOW *stdscr;                 /* The standard screen to draw on */
-int maxy, maxx;                 /* Maximum values of rows and columns */
+int maxy, maxx;                 /* Maximum values of screen rows and columns */
 WINDOW *editor_window;          /* The editor window */
 WINDOW *cmd_window;             /* The command line window/field */
 
