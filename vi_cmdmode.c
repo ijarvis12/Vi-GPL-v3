b4bd1977ce6484a7f74529892983f5e9b1fd5c49
Ian Jarvis <ijarvis12@gmail.com>
2023-12-16 13:48:38 -0600

    Update vi_cmdmode.c

--- vi_cmdmode.c
+++ vi_cmdmode.c
@@ -1,4 +1,5 @@
 #include "vi_cmdmode.h"
+#include <string.h>
 
 void
 commandmode_main()
@@ -10,6 +11,15 @@ commandmode_main()
         /* Process command */
         char first_char = command[0];
         switch (first_char) {
+                case ':':
+                        /* Get rest of command */
+                        char rest_command[maxx];
+                        for(char i=1; i<strlen(command); i++) rest_command[i-1] = command[i];
 
+                        /* Do rest of command */
+                        char second_char = rest_command[0];
+                        switch (seocnd_char) {
+                                case 'x':
+                        }
         }
 }
