a6b7abda10c2f0080394c091ea47b743490f099a
Ian Jarvis <ijarvis12@gmail.com>
2023-12-16 13:56:49 -0600

    Update vi.h

--- vi.h
+++ vi.h
@@ -9,11 +9,15 @@ WINDOW *cmd_window;             /* The command line window/field */
 struct Buffers {
         struct Buffer {
                 struct Lines {
-                        char[512] line;
+                        char[256] line;
                 } lines[maxy-1];
         } buffer[32];
 } buffers;
 
-extern void commandmode_main();     /* Command mode main function */
-extern void insertmode_main();     /* Insert mode main function */
+unsigned char current_buffer;   /* The current buffer number */
+unsigned char current_line;     /* The current line in the buffer */
+unsigned char current_column;   /* The current column in the current line */
+
+extern void commandmode_main(); /* Command mode main function */
+extern void insertmode_main();  /* Insert mode main function */
 extern void visualmode_main();  /* Visual mode main function */
