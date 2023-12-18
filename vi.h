852a62443ca234328bec2cd053ef147605331864
Ian Jarvis <ijarvis12@gmail.com>
2023-12-18 14:33:02 -0600

    Update vi.h

--- vi.h
+++ vi.h
@@ -6,6 +6,7 @@ WINDOW *editor_window;          /* The editor window */
 WINDOW *cmd_window;             /* The command line window/field */
 
 FILE *file;                     /* The file to read into buffer 0 */
+long int file_pos;              /* The position in the file */
 
 /* The text buffers */
 struct Buffers {
@@ -16,8 +17,8 @@ struct Buffers {
         } buffer[27];           /* File buffer (0) plus buffers a-z (1-26) */
 } buffers;
 
-unsigned char current_line;     /* The current line in the buffer */
-unsigned char current_column;   /* The current column in the current line */
+int ypos;                       /* The current line in the buffer */
+int xpos;                       /* The current column in the current line */
 bool work_saved;                /* Whether the file is saved or not */
 
 extern void commandmode_main(); /* Command mode main function */
