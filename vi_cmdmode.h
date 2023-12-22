623e332567b2fd4c9788a8b5b55e1a3664a353bf
Ian Jarvis <ijarvis12@gmail.com>
2023-12-22 00:19:58 -0600

    Update vi_cmdmode.h

--- vi_cmdmode.h
+++ vi_cmdmode.h
@@ -1,6 +1,1 @@
-#include "vi.h"
-#include "re.h"
 
-void commandmode_main(char *); /* Main entry point for command mode */
-void write_to_file(char *);    /* Write file to storage */
-void quit();                   /* Quit out of current file buffer, and maybe program */
