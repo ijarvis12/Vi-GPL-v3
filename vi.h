c791066f33370101f4e09d1bbbca862e9d666ae8
Ian Jarvis <ijarvis12@gmail.com>
2024-01-10 11:31:37 -0600

    Update vi.h

--- vi.h
+++ vi.h
@@ -24,28 +24,28 @@ typedef WINDOW* GWINDOW*;
 typedef FILE* GFILE*;
 
 
-unsigned gchar MAX_FILES = 32;               /* The total number of possible open files */
+unsigned gchar MAX_FILES = 32;      /* The total number of possible open files */
 
-GWINDOW *stdscr;                             /* The standard screen to draw on */
-gint maxy, maxx;                             /* Maximum values of screen rows and columns */
-GWINDOW *editor_windows[MAX_FILES];          /* The editor windows */
-GWINDOW *command_window;                     /* The command line window/field */
+GWINDOW *stdscr;                    /* The standard screen to draw on */
+gint maxy, maxx;                    /* Maximum values of screen rows and columns */
+GWINDOW *editor_windows[MAX_FILES]; /* The editor windows */
+GWINDOW *command_window;            /* The command line window/field */
 
-unsigned gchar g;                            /* The file number, can have multiple open */
-GFILE *files[MAX_FILES];                     /* The file pointer(s) to read into temp files */
-GFILE *temp_files[MAX_FILES];                /* The temporary file pointer(s) to write to */
-gchar *file_names[MAX_FILES];                /* The name(s) of the file(s) to load */
-gchar *temp_file_names[MAX_FILES];           /* The names(s) of the temporary file(s) to edit */
+unsigned gchar g;                  /* The file number, can have multiple open */
+GFILE *files[MAX_FILES];           /* The file pointer(s) to read into temp files */
+GFILE *temp_files[MAX_FILES];      /* The temporary file pointer(s) to write to */
+gchar *file_names[MAX_FILES];      /* The name(s) of the file(s) to load */
+gchar *temp_file_names[MAX_FILES]; /* The name(s) of the temporary file(s) to edit */
 
-gbool buffer_is_open[MAX_FILES];             /* Whether the buffer is open or not */
-gbool work_saved[MAX_FILES];                 /* Whether the file is saved or not */
+gbool buffer_is_open[MAX_FILES];   /* Whether the buffer is open or not */
+gbool work_saved[MAX_FILES];       /* Whether the file is saved or not */
 
-gint ypos[MAX_FILES];                        /* The current line in the buffer screen */
-gint xpos[MAX_FILES];                        /* The current column in the current line */
+gint ypos[MAX_FILES];    /* The current line in the buffer screen */
+gint xpos[MAX_FILES];    /* The current column in the current line */
 
-gvoid print(gchar *);                        /* Print a message to the bottom of screen */
-gvoid error(gchar *);                        /* Print an error message to the bottom screen */
+gvoid print(gchar *);    /* Print a message to the bottom of screen */
+gvoid error(gchar *);    /* Print an error message to the bottom screen */
 
-extern gvoid commandmode_main(gchar *);      /* Command mode main function */
-extern gvoid insertmode_main(gchar *);       /* Insert mode main function */
-extern gvoid visualmode_main();              /* Visual mode main function */
+extern gvoid commandmode_main(gchar *);       /* Command mode main function */
+extern gvoid insertmode_main(gchar, gchar *); /* Insert mode main function */
+extern gvoid visualmode_main();               /* Visual mode main function */
