80eab96dbebdd8a682ebb759beb33ebce7bf9fd7
Ian Jarvis <ijarvis12@gmail.com>
2024-01-11 09:15:38 -0600

    Update vi_insmode.c

--- vi_insmode.c
+++ vi_insmode.c
@@ -9,33 +9,33 @@ insertmode_main(gchar cmd, gchar *chs)
   switch(cmd) {
 
     case 'i':
-      if(strlen(chs) > 0) insert_chs_after_cursor(chs);
-      else insert_chs_after_cursor("");
+      if(strlen(chs) > 0) insert_chs('b', chs);
+      else insert_chs('b', ""); /* 'b' == before cursor */
       break;
 
     case 'I':
       move_cursor_before_line();
-      insert_chs_after_cursor("");
+      insert_chs('b', "");
       break;
 
     case 'a':
       move_cursor_over_one();
-      insert_chs_after_cursor("");
+      insert_chs('a', ""); /* 'a' == after cursor */
       break;
 
     case 'A':
       move_cursor_after_line();
-      insert_chs_after_cursor("");
+      insert_chs('a', "");
       break;
 
     case 'o':
       open_newline_after_current_line();
-      insert_chs_after_cursor("");
+      insert_chs('b', "");
       break;
 
     case 'O':
       open_newline_before_current_line();
-      insert_chs_after_cursor("");
+      insert_chs('b', "");
       break;
 
     case 'R':
