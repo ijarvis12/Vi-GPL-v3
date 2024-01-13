8a315009bd5657918d7c53a765332ec10245e451
Ian Jarvis <ijarvis12@gmail.com>
2024-01-13 10:26:56 -0600

    Update vi.h

--- vi.h
+++ vi.h
@@ -15,13 +15,17 @@ typedef unsigned char unsigned gchar;
 typedef short gshort;
 typedef unsigned short unsigned gshort;
 typedef int gint;
+typedef int* gint*;
 typedef unsigned int unsigned gint;
+typedef unsigned int* unsigned gint*;
 typedef float gfloat;
 typedef long int long gint;
 typedef unsigned long int unsigned long gint;
 typedef double gdouble;
 typedef WINDOW* GWINDOW*;
+typedef WINDOW** GWINDOW**;
 typedef FILE* GFILE*;
+typedef FILE** GFILE**;
 
 
 unsigned gchar MAX_FILES = 32;      /* The total number of possible open files */
@@ -37,11 +41,13 @@ GFILE *temp_files[MAX_FILES];      /* The temporary file pointer(s) to write to 
 gchar *file_names[MAX_FILES];      /* The name(s) of the file(s) to load */
 gchar *temp_file_names[MAX_FILES]; /* The name(s) of the temporary file(s) to edit */
 
+unsigned gchar buffer_number;      /* Yank and paste buffer number */
+*gchar buffers[27];                /* Yank and paste buffers 'a' - 'z' plus a default zero */
 gbool buffer_is_open[MAX_FILES];   /* Whether the buffer is open or not */
 gbool work_saved[MAX_FILES];       /* Whether the file is saved or not */
 
-gint ypos[MAX_FILES];    /* The current line in the buffer screen */
-gint xpos[MAX_FILES];    /* The current column in the current line */
+unsigned gint ypos[MAX_FILES];    /* The current line in the buffer screen */
+unsigned gint xpos[MAX_FILES];    /* The current column in the current line */
 
 gvoid print(gchar *);    /* Print a message to the bottom of screen */
 gvoid error(gchar *);    /* Print an error message to the bottom screen */
