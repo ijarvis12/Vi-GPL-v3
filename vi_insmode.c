a8e7b63379fcf6bfb203d2c1676717bfc876a53a
Ian Jarvis <ijarvis12@gmail.com>
2024-02-10 13:52:55 -0600

    Update vi_insmode.c

--- vi_insmode.c
+++ vi_insmode.c
@@ -1,41 +1,41 @@
 #include "vi.h"
 
-gvoid insert_chs_after_cursor(gchar *);
+gvoid insert_chars(ghcar, gchar *);
 
 gvoid
-insertmode_main(gchar cmd, gchar *chs)
+insertmode_main(gchar command, gchar *chars)
 {
   
-  switch(cmd) {
+  switch(command) {
 
     case 'i':
-      if(strlen(chs) > 0) insert_chs('b', chs);
-      else insert_chs('b', ""); /* 'b' == before cursor */
+      if(strlen(chs) > 0) insert_chars('b', chars); /* 'b' == before cursor */
+      else insert_chars('b', "");
       break;
 
     case 'I':
       move_cursor_before_line();
-      insert_chs('b', "");
+      insert_chars('b', "");
       break;
 
     case 'a':
       move_cursor_over_one();
-      insert_chs('a', ""); /* 'a' == after cursor */
+      insert_chars('a', ""); /* 'a' == after cursor */
       break;
 
     case 'A':
       move_cursor_after_line();
-      insert_chs('a', "");
+      insert_chars('a', "");
       break;
 
     case 'o':
       open_newline_after_current_line();
-      insert_chs('b', "");
+      insert_chars('b', "");
       break;
 
     case 'O':
       open_newline_before_current_line();
-      insert_chs('b', "");
+      insert_chars('b', "");
       break;
 
     case 'R':
