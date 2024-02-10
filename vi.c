7494f5d71ada6377612ff30e480d225e5b91eb62
Ian Jarvis <ijarvis12@gmail.com>
2024-02-10 15:42:15 -0600

    Update vi.c

--- vi.c
+++ vi.c
@@ -122,7 +122,7 @@ main(gint argc, gchar *argv[])
   while(true) {
     wgetyx(editor_window[g], ypos[g], xpos[g]);
     visual_command = wgetch(editor_window[g]); // the command
-    count[2] = {0, 0}; // prefix number(s) for commands
+    count = {0, 0}; // prefix number(s) for commands
     ascii_buffer_number = 0; // 'a' - 'z' in ascii numbers plus a default '0' for undo
     visualmode_main(visual_command);
   }
