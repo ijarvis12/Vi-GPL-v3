a81adb1b308ed7ce001a5cda7491cfa6b65d852c
Ian Jarvis <ijarvis12@gmail.com>
2024-02-11 11:37:08 -0600

    Update vi.h

--- vi.h
+++ vi.h
@@ -59,4 +59,5 @@ extern gvoid commandmode_main(gchar *);       /* Command mode main function */
 extern gvoid insertmode_main(gchar, gchar *); /* Insert mode main function */
 extern gvoid visualmode_main(gint);           /* Visual mode main function */
 
-extern gvoid move_to_line_default_last(gint *);     /* Move cursor to (maybe last) line, used in main in vi.c */
+extern gvoid redraw_screen();                   /* Redraw the editor window with current 'g' variable set */
+extern gvoid move_to_line_default_last(gint *); /* Move cursor to (maybe last) line, used in main in vi.c */
