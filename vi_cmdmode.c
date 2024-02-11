2ed1cd0f0420adb5dbcd55befc3c90d559cc48b4
Ian Jarvis <ijarvis12@gmail.com>
2024-02-11 11:39:09 -0600

    Update vi_cmdmode.c

--- vi_cmdmode.c
+++ vi_cmdmode.c
@@ -291,6 +291,7 @@ commandmode_main(gchar *input_command) /* Main entry point for command mode */
   }
   free(input_command);
   free(command);
+  redraw_screen();
   return; /* For sanity, should go back to visual mode */
 }
 
