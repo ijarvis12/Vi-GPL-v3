d4bd212e42568cc845962ede5b1136a9a7ccd62b
Ian Jarvis <ijarvis12@gmail.com>
2024-02-25 11:45:17 -0600

    Update vi_insmode.c

--- vi_insmode.c
+++ vi_insmode.c
@@ -12,29 +12,31 @@ gvoid insertmode_main(gchar command, gchar *chars) {
 
     case 'I':
       visualmode_main('^'); /* move cursor to start of line */
-      insert_chars("");
+      if(insert_chars("")) next_gtemp();
       break;
 
     case 'a':
       visualmode_main('l');
-      insert_chars("");
+      if(insert_chars("")) next_gtemp();
       break;
 
     case 'A':
       visualmode_main('$'); /* move cursor to end of line */
-      insert_chars("");
+      if(insert_chars("")) next_gtemp();
       break;
 
     case 'o':
       visualmode_main('$');  /* open newline after current line */
       insert_chars({10, 0}); /*              "                  */
       insert_chars("");
+      next_gtemp(); /* Because of newline insert must get next gtemp */
       break;
 
     case 'O':
       visualmode_main('|');  /* open newline before current line */
       insert_chars({10, 0}); /*              "                   */
       insert_chars("");
+      next_gtemp(); /* Because of newline insert must get next gtemp */
       break;
 
     default:
@@ -42,11 +44,13 @@ gvoid insertmode_main(gchar command, gchar *chars) {
 
   }
 
-  work_saved[g] = false;
   wgetyx(editor_window[g], ypos[g], xpos[g]);
-
+  return;
 }
 
 gbool insert_chars(gchar *chars) { /* Returns 'true' if changes made, else false */
 
+
+  work_saved[g] = false;
+  return true;
 }
