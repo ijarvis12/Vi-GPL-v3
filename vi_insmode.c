8a9cd817985d869d2452fc581398f097875cba85
Ian Jarvis <ijarvis12@gmail.com>
2024-02-28 09:20:05 -0600

    Update vi_insmode.c

--- vi_insmode.c
+++ vi_insmode.c
@@ -67,27 +67,52 @@ gvoid next_gtemp() {
       sprintf(num, "%u", gtemp[g]);
       error(strcat(strcat(message, num), " for undo, changes will not save"));
     }
-    else { /* Else */
+    else { /* Else if file opens... */
+      unsigned long gint temp_pos = ftell(gbuffer[g].gtemp_files[gtemp[g]-1]);
+      gchar **line;
+      while(getline(line, NULL, gbuffer[g].gtemp_files[gtemp[g]-1]) > 0) fprintf(gbuffer[g].gtemp_files[gtemp[g]], "%s", *line);
+      fseek(gbuffer[g].gtemp_files[gtemp[g]-1], temp_pos, SEEK_SET);
+      fseek(gbuffer[g].gtemp_files[gtemp[g]], temp_pos, SEEK_SET);
+      free(line);
       gbuffer[g].ypos[gtemp[g]] = gbuffer[g].ypos[gtemp[g]-1];
       gbuffer[g].xpos[gtemp[g]] = gbuffer[g].xpos[gtemp[g]-1];
       fseek(gbuffer[g].gtemp_files[gtemp[g]], ftell(gbuffer[g].gtemp_files[gtemp[g]-1]), SEEK_SET);
     }
   }
   else {
+    /* Close and delete zero temp file */
     fclose(gbuffer[g].gtemp_files[0]);
     unlink(gbuffer[g].gtemp_file_names[0]);
+    unsigned long gint temp_pos;
     gchar num[4];
-    for(unsigned gchar i=0; i<GUNDO_MAX-1; i++) {
+    /* For the rest of the temp files, move the info back one */
+    for(unsigned gchar i=0; i<GUNDO_MAX-2; i++) {
+      /* Save position in file */
+      temp_pos = ftell(gbuffer[g].gtemp_files[i+1]);
+      /* Copy name over, and give it the right name */
       strcpy(gbuffer[g].gtemp_file_names[i], gbuffer[g].gtemp_file_names[i+1]);
       sprintf(num, "%u", i);
       gbuffer[g].gtemp_file_names[i][strlen(gbuffer[g].gtemp_file_names[i])-1] = num;
-      memcpy(gbuffer[g].gtemp_files[i], gbuffer[g].gtemp_files[i+1], sizeof(gbuffer[g].gtemp_files[i+1]));
+      rename(gbuffer[g].gtemp_file_names[i+1], gbuffer[g].gtemp_file_names[i]);
+      /* Close incremented file and open it again under current file number */
+      fclose(gbuffer[g].gtemp_files[i+1]);
+      gbuffer[g].gtemp_files[i] = fopen(gbuffer[g].gtemp_file_names[i], "rw");
+      /* Copy data over and file seek */
+      fseek(gbuffer[g].gtemp_files[i], temp_pos, SEEK_SET);
       gbuffer[g].ypos[i] = gbuffer[g].ypos[i+1];
       gbuffer[g].xpos[i] = gbuffer[g].xpos[i+1];
       gbuffer[g].gtop_line[i] = gbuffer[g].top_line[i+1];
       gbuffer[g].gtotal_lines[i] = gbuffer[g].gtotal_lines[i+1];
+      
     }
+    /* Finally open current file and copy over data */
+    temp_pos = ftell(gbuffer[g].temp_files[gtemp[g]-1]);
     gbuffer[g].gtemp_files[gtemp[g]] = fopen(gbuffer[g].gtemp_file_names[gtemp[g]], "rw");
+    gchar **line;
+    while(getline(line, NULL, gbuffer[g].gtemp_files[gtemp[g]-1]) > 0) fprintf(gbuffer[g].gtemp_files[gtemp[g]], "%s", *line);
+    free(line);
+    fseek(gbuffer[g].gtemp_files[gtemp[g]-1], temp_pos, SEEK_SET);
+    fseek(gbuffer[g].gtemp_files[gtemp[g]], temp_pos, SEEK_SET);
     gbuffer[g].ypos[gtemp[g]] = gbuffer[g].ypos[gtemp[g]-1];
     gbuffer[g].xpos[gtmep[g]] = gbuffer[g].xpos[gtemp[g]-1];
     gbuffer[g].gtop_line[gtemp[g]] = gbuffer[g].top_line[gtemp[g]-1];
