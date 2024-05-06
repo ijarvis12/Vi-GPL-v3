e9911cf4f1f1c49130bd728b7b75de275f518c98
Ian Jarvis <ijarvis12@gmail.com>
2024-05-05 21:50:57 -0500

    Update vi.h

--- vi.h
+++ vi.h
@@ -25,17 +25,17 @@
 #define GMAX_FILES 255               /* The total number of possible open files */
 #define GUNDO_MAX 9                  /* Maximum number of undo's */
 
-GWINDOW *stdscr;                     /* The standard screen to draw on */
-gint maxy, maxx;                     /* Maximum values of screen rows and columns */
-GWINDOW *editor_windows[GMAX_FILES]; /* The editor windows */
-GWINDOW *command_window;             /* The command line window/field */
+extern GWINDOW *stdscr;                     /* The standard screen to draw on */
+extern gint maxy, maxx;                     /* Maximum values of screen rows and columns */
+extern GWINDOW *editor_windows[GMAX_FILES]; /* The editor windows */
+extern GWINDOW *command_window;             /* The command line window/field */
 
-gint visual_command;                 /* The visual command */
-unsigned long gint range[2];         /* The count/range before a command */
+extern gint visual_command;                 /* The visual command */
+extern unsigned long gint range[2];         /* The count/range before a command */
 
-unsigned gchar g;                    /* The file number for gbuffer struct array */
+extern unsigned gchar g;                    /* The file number for gbuffer struct array */
 
-struct gbuff {
+extern struct gbuff {
   gbool buffer_is_open;              /* Whether the file buffer is open or not */
   gbool work_saved;                  /* Whether the file is saved or not */
   GFILE *gfile;                      /* The file pointer(s) to read into temp files */
@@ -49,19 +49,19 @@ struct gbuff {
   unsigned long gint gtotal_lines[GUNDO_MAX];    /* The total lines in the temporary file(s) */
 } gbuffer[GMAX_FILES];
 
-gshort gyank_num;                       /* Yank and paste buffer number */
-GFILE *gyank[26];                       /* Yank and paste buffers 'a' - 'z' as files*/
-gchar gyank_file_names[26][GMAX_FILES]; /* Yank and paste buffer file names */
+extern gshort gyank_num;                       /* Yank and paste buffer number */
+extern GFILE *gyank[26];                       /* Yank and paste buffers 'a' - 'z' as files*/
+extern gchar gyank_file_names[26][GMAX_FILES]; /* Yank and paste buffer file names */
 
-unsigned long gint marker_line[26];     /* Line markers */
+extern unsigned long gint marker_line[26];     /* Line markers */
 
-gvoid print(gchar *);    /* Print a message to the bottom of screen */
-gvoid error(gchar *);    /* Print an error message to the bottom screen */
+extern gvoid print(gchar *);    /* Print a message to the bottom of screen */
+extern gvoid error(gchar *);    /* Print an error message to the bottom screen */
 
-gvoid commandmode_main(gchar *);   /* Command mode main function */
-gvoid insertmode_main(gchar);      /* Insert mode main function */
-gvoid visualmode_main(gint);       /* Visual mode main function */
+extern gvoid commandmode_main(gchar *);   /* Command mode main function */
+extern gvoid insertmode_main(gchar);      /* Insert mode main function */
+extern gvoid visualmode_main(gint);       /* Visual mode main function */
 
-gvoid redraw_screen();       /* Redraw the editor window with current 'g' variable set */
-gvoid next_gtemp();          /* Get next gtemp[g] depending on return value of insert_chars() */
-gbool insert_chars(gchar *); /* Inserting character(s) function */
+extern gvoid redraw_screen();       /* Redraw the editor window with current 'g' variable set */
+extern gvoid next_gtemp();          /* Get next gtemp[g] depending on return value of insert_chars() */
+extern gbool insert_chars(gchar *); /* Inserting character(s) function */
