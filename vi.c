936377fb7439f4c918aa3068f4be97eddea5807a
Ian Jarvis <ijarvis12@gmail.com>
2024-05-06 23:37:35 -0500

    Update vi.c

--- vi.c
+++ vi.c
@@ -190,7 +190,7 @@ gvoid print(gchar *output) {
   mvwhline(command_window, 0, 0, ' ', maxx);
   mvwaddstr(command_window, 0, 0, output);
   wrefresh(command_window);
-  sleep(5);
+  sleep(3);
   return;
 }
 
