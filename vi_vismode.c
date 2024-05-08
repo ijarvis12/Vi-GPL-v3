d69ed21f15d48a428623de9be95e567f9798bd63
Ian Jarvis <ijarvis12@gmail.com>
2024-05-07 20:27:17 -0500

    Update vi_vismode.c

--- vi_vismode.c
+++ vi_vismode.c
@@ -840,7 +840,8 @@ gvoid visualmode_main(gint visual_command) {
         for(gint j=len_undo; j>0; j--) {
           gbuffer[g].gtemp_file_names[strlen(gbuffer[g].gtemp_file_names)-j] = gundo_str[len_undo-j];
         }
-        gbuffer[g].gtemp_files = fopen(gbuffer[g].gtemp_file_names);
+        gbuffer[g].gtemp_files = fopen(gbuffer[g].gtemp_file_names, "r+");
+        if(gbuffer[g].gtemp_files == NULL) error("Couldn't open previous undo file");
         redraw_screen();
       }
       break;
